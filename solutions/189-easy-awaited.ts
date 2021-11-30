dc49cd03e326c192dcb79ddd58be137bf565323b
Gabriel de Oliveira <gabriel.o03@aluno.ifsc.edu.br>
2021-11-30 11:17:05 -0300

    Awaited

---
+++ solutions/189-easy-awaited.ts
@@ -0,0 +1,49 @@
+/*
+  189 - Awaited
+  -------
+  by Maciej Sikora (@maciejsikora) #easy #promise #built-in
+
+  ### Question
+
+  If we have a type which is wrapped type like Promise. How we can get a type which is inside the wrapped type? For example if we have `Promise<ExampleType>` how to get ExampleType?
+
+  > This question is ported from the [original article](https://dev.to/macsikora/advanced-typescript-exercises-question-1-45k4) by [@maciejsikora](https://github.com/maciejsikora)
+
+  > View on GitHub: https://tsch.js.org/189
+*/
+
+
+/* _____________ Your Code Here _____________ */
+
+// note: this won't pass the error check, but it does reflect real world usage
+// of await since awaiting on a non-promise value simply returns the value.
+// adding a constraint to T would require a solution that is not recursive (and
+// likely unnecessarily complex).
+type MyAwaited<T> = T extends Promise<infer U> ? MyAwaited<U> : T
+
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '@type-challenges/utils'
+
+type X = Promise<string>
+type Y = Promise<{ field: number }>
+type Z = Promise<Promise<string | number>>
+
+type cases = [
+  Expect<Equal<MyAwaited<X>, string>>,
+  Expect<Equal<MyAwaited<Y>, { field: number }>>,
+  Expect<Equal<MyAwaited<Z>, string | number>>,
+]
+
+// @ts-expect-error
+type error = MyAwaited<number>
+
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/189/answer
+  > View solutions: https://tsch.js.org/189/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
